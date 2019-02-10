b48777f52247ca92d79204bb0b6562566cbfba2e
Freddie Sackur <github@dustyfox.uk>
2019-02-10 17:39:56 +0000

    WIP

--- Invoke-InNewRunspace.ps1
+++ Invoke-InNewRunspace.ps1
@@ -12,40 +12,39 @@
 
     Adjust the path to Invoke-InNewRunspace.ps1 to suit your layout.
 #>
+using namespace System.Collections.Generic
+using namespace System.Management.Automation
 
 if ([runspace]::DefaultRunspace.Name -notmatch 'Pester')
 {
     $ISS = [initialsessionstate]::CreateDefault()
-       
+    $ISS.ImportPSModule('Pester')
+    $ISS.UseFullLanguageModeInDebugger = $true
+
     $RS = [runspacefactory]::CreateRunspace($Host, $ISS)
     $RS.Name = "Pester" + [datetime]::Now.ToString('s')
-    
+
     $RS.Open()
+    $RS.Debugger.SetDebugMode('LocalScript')
+
+    $Frame = (Get-PSCallStack)[1]
+    $TestBreakpoints = Get-PSBreakpoint -Script $Frame.InvocationInfo.MyCommand.Source
+    $RS.Debugger.SetBreakpoints([List[Breakpoint]]$TestBreakpoints)
 
     $PS = [Powershell]::Create()
     $PS.Runspace = $RS
-
 
     $null = $PS.
         AddScript('Write-Verbose "Running in runspace $([runspace]::DefaultRunspace.Name)" -Verbose').
         AddStatement().
         AddCommand("Set-Location").
-        AddParameter('Path', $PWD.Path).
-        AddStatement().
-        AddCommand('Import-Module').
-        AddParameter('Name', 'Pester')
+        AddParameter('Path', $PWD.Path)
 
     $PS.Invoke()
     $PS.Commands.Clear()
 
 
-    $TestBreakpoints = Get-PSBreakpoint | Where-Object {$_.Script -eq $Frame.InvocationInfo.MyCommand.Source}
-    $TestBreakpoints | ForEach-Object {
-        $null = $PS.
-            AddCommand('Set-PSBreakpoint').
-            AddParameter('Script', $_.Script).
-            AddParameter('Line', $_.Line)
-    }
+
 
     Get-Variable *Preference |
         Where-Object {$_.Name -notmatch '^WhatIf|^Confirm'} |
@@ -68,21 +67,21 @@ if ([runspace]::DefaultRunspace.Name -notmatch 'Pester')
     }
 
 
-    $Frame = (Get-PSCallStack)[1] #| Out-String | Write-Host
+
     $Invocation = $Frame.InvocationInfo.MyCommand.Source
-    
+
     $null = $PS.AddScript($Invocation)
 
-    
+
     $PS.Invoke()
 
-    
     $PS.Streams.Error       | ForEach-Object {Write-Error $_}
     $PS.Streams.Warning     | ForEach-Object {Write-Warning $_}
     $PS.Streams.Verbose     | ForEach-Object {Write-Verbose $_}
     $PS.Streams.Debug       | ForEach-Object {Write-Debug $_}
     $PS.Streams.Information | ForEach-Object {Write-Information $_}
 
+    $RS.Dispose()
     $PS.Dispose()
 
 
